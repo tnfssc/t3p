2ed40f244a2fadc7210242de14fcf0c6080c84e1
Sharath <sharath@saaslabs.co>
2023-01-07 18:22:33 +0530

    feat: PWAified

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
 import { defineConfig } from "vitest/config";
 import ReactPlugin from "@vitejs/plugin-react-swc";
 import { ViteAliases } from "vite-aliases";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   test: {
     environment: "jsdom",
   },
-  plugins: [ReactPlugin(), ViteAliases()],
+  plugins: [
+    ReactPlugin(),
+    ViteAliases(),
+    VitePWA({
+      manifest: {
+        name: "t3p",
+        short_name: "t3p",
+        description: "t3p",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "assets/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "assets/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "assets/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
