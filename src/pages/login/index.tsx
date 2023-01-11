9990ab912fb06a99a87e0fc061df515ca9582b46
Sharath <sharath@saaslabs.co>
2023-01-11 21:27:25 +0530

    fix: Login not working on phones

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -17,37 +17,32 @@ const LoginPage = () => {
   if (!googleAuthProvider) throw new Error("googleAuthProvider not found");
   authProviders = authProviders.filter((p) => p.name !== "google");
 
-  const handleLogin =
-    (authProvider: AuthProviderInfo) =>
-    (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-      e.preventDefault();
-      localStorage.setItem("authProvider", JSON.stringify(authProvider));
-      window.location.href = authProvider.authUrl + authCallbackUrl;
-    };
+  const handleLogin = (authProvider: AuthProviderInfo) => () => {
+    localStorage.setItem("authProvider", JSON.stringify(authProvider));
+    window.location.href = authProvider.authUrl + authCallbackUrl;
+  };
 
   if (user) return <Redirect to="/" />;
 
   return (
     <div className="flex flex-col place-items-center">
       <h1 className="text-4xl mb-4">Login</h1>
-      <a
-        href={googleAuthProvider.authUrl + authCallbackUrl}
+      <button
         onClick={handleLogin(googleAuthProvider)}
-        onContextMenu={(e) => e.preventDefault()}
+        onTouchEnd={handleLogin(googleAuthProvider)}
         className="btn outline-blue-500 outline-4 bg-white text-black normal-case hover:bg-blue-300"
       >
         <FcGoogle className="inline-block w-6 h-6 mr-2" />
         Login with Google
-      </a>
+      </button>
       {authProviders?.map((provider) => (
-        <a
-          href={provider.authUrl + authCallbackUrl}
+        <button
           onClick={handleLogin(provider)}
-          onContextMenu={(e) => e.preventDefault()}
+          onTouchEnd={handleLogin(googleAuthProvider)}
           className="btn"
         >
           Login with {provider.name}
-        </a>
+        </button>
       ))}
     </div>
   );
